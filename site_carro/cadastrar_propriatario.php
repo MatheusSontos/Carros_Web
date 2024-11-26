9bd10e9dc3de3d7369f295fa422e1d7008fa9e2d
Matheus Pereira Santos <125597672+MatheusSontos@users.noreply.github.com>
2024-11-26 18:34:05 -0300

    Add files via upload

---
+++ site_carro/cadastrar_propriatario.php
@@ -0,0 +1,21 @@
+<h1>Cadastrar Proprietário</h1>
+
+<form action="?page=salvar_proprietario" method="post">
+    <input type="hidden" name="acao" value="cadastrar">
+
+<div class="card" style="width: 30rem;">
+    <div>
+        <div class="container bm-3">
+            <label class="mt-3">Proprietário</label>
+            <input type="text" name="proprietario" class="from-control" placeholder="Digite o nome do Proprietário:">
+        </div>
+        <div class="container bm-3">
+            <label class="mt-3">RG</label>
+            <input type="text" name="rg-propri" class="from-control" placeholder="Digite o RG do Proprietário:">
+        </div>
+        <div class="container bm-3">
+            <label class="mt-3">CPF</label>
+            <input type="text" name="cpf-propri" class="from-control" placeholder="Digite o CPF do Proprietário:">
+        </div>
+    </div>
+</div>
---
+++ site_carro/listar_proprietario.php
@@ -0,0 +1,1 @@
+<h1>Listar Proprietário</h1>
